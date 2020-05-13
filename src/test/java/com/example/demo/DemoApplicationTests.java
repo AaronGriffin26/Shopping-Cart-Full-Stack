4aed46ed50da88f36442694ce00d8c183d9b685d
Carmen <Carmen.smith490@gmail.com>
2020-05-13 11:22:15 -0400

    gitflow-feature-stash: FrontEnd-still missing changes

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.shoppingcartfullstack;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ShoppingCartFullStackApplicationTests {
+class DemoApplicationTests {
 
     @Test
     void contextLoads() {
