76eb66c0b141fdf829a2d747ed058998e5e8455d
Aaron Griffin <griffin.aaron.r@gmail.com>
2020-05-14 00:00:42 -0400

    Modified login section

--- frontend/src/Login/NavigationComponent.jsx
+++ frontend/src/Login/NavigationComponent.jsx
@@ -3,11 +3,24 @@ import {Link} from "react-router-dom";
 
 export default class Navigation extends Component {
     render() {
+        let loginOptions;
+        if (false)// logged in
+            loginOptions = (
+                <div id="login">
+                    <Link to="/log-out">Log Out</Link>
+                </div>
+            );
+        else
+            loginOptions = (
+                <div id="login">
+                    <Link to="/sign-in">Log In</Link>
+                    <Link to="/sign-up">Sign Up</Link>
+                </div>
+            );
         return (
             <div>
                 <Link to="/">Home</Link>
-                <Link id="login" to="/sign-in">Log In</Link>
-                <Link id="sign-up" to="/sign-up">Sign Up</Link>
+                {loginOptions}
             </div>
         );
     }
